37b5ec76767be9c36cd67b2397b6e7557b7fac2f
nianxi4563 <62866097+nianxi666@users.noreply.github.com>
2025-09-29 17:38:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,20 @@ import torch
 
 # Load the model
 model_id = "./HunyuanImage-3"
-# Currently we can not load the model using HF model_id `tencent/HunyuanImage-3.0` directly 
-# due to the dot in the name.
 
 kwargs = dict(
-    attn_implementation="sdpa",     # Use "flash_attention_2" if FlashAttention is installed
+    attn_implementation="sdpa",
     trust_remote_code=True,
     torch_dtype="auto",
     device_map="auto",
-    moe_impl="eager",   # Use "flashinfer" if FlashInfer is installed
+    moe_impl="eager",
 )
 
-# Load the model and explicitly move all its components to the GPU
+# Load the model AND explicitly move all its components to the GPU
 model = AutoModelForCausalLM.from_pretrained(model_id, **kwargs).to("cuda")
 model.load_tokenizer(model_id)
 
-# generate the image
+# Generate the image
 prompt = "A brown and white dog is running on the grass"
 image = model.generate_image(prompt=prompt, stream=True)
 image.save("image.png")
